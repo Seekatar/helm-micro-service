86ca99f78bf500e280215817893932e44e2303d1
Jim Wallace <seekatar@gmail.com>
2021-01-25 17:21:24 -0500

    working w/proper override

---
+++ run.ps1
@@ -0,0 +1,36 @@
+
+param(
+    [ValidateSet('install','uninstall','dry-run')]
+    [string[]] $Task,
+    [string] $name = 'cas-widget-api',
+    [string] $overrideFile = '..\config-values-api.yml'
+)
+
+function exec([Parameter(Mandatory)] [string] $taskName,
+              [Parameter(Mandatory)] [ScriptBlock] $scriptBlock,
+              [string] $WorkingDirectory ) {
+
+    try {
+        if ($WorkingDirectory) { Push-Location $WorkingDirectory }
+        & $scriptBlock
+        if ($LASTEXITCODE -ne 0) { throw "Task '$taskName' exited with $LASTEXITCODE" }
+    } finally {
+        if ($WorkingDirectory) { Pop-Location}
+    }
+}
+
+foreach ($t in $Task) {
+    switch ($t) {
+        'install' {
+            exec $t { helm upgrade --install --values $overrideFile $name . } -workingdir $PSScriptRoot
+        }
+        'uninstall' {
+            exec $t { helm uninstall $name }
+        }
+        'dry-run' {
+            exec $t { helm install . --dry-run  --generate-name --values $overrideFile | ..\Split-Debug.ps1 -Outputpath \temp\helm } -WorkingDirectory $PSScriptRoot
+        }
+        Default {}
+    }
+}
+
